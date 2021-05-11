c3bf40c68052942206c360f77e45e96d99c15262
Kumar Atul jaiswal <31371130+whoiskumaratul@users.noreply.github.com>
2021-05-11 13:33:31 +0530

    Update geo.py

--- geo.py
+++ geo.py
@@ -78,7 +78,7 @@ def printRecord(ip):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='GeoOccupy is command line argument tool. GeoOccupy means geolocation finder, which is used to find country, city, continent, country code, postal, time-zone via IP or Host. \n', prog='python3 geo2.py all',  usage='%(prog)s [options]')
+    parser = argparse.ArgumentParser(description='GeoOccupy is command line argument tool. GeoOccupy means geolocation finder, which is used to find country, city, continent, country code, postal, time-zone via IP or Host. \n', prog='python3 geo2.py -ip=$IP or $HOST -c= -cit= -con= -code=  -postal= -asn= -zone= -metro= -latitude= -longitude= -dma=',  usage='%(prog)s [options]')
     parser.add_argument('-ip', action = 'store', dest='ip', help='IP or Host name that you want to extract info', required=True)
     parser.add_argument('-c', action = 'store', dest='country', help='Find country name')#, required=True)
     parser.add_argument('-cit', action = 'store', dest='city', help='Find city name' )#, required=True)
