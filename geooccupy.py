a60c9ae35f7f9eeb1a19f41c7d5a1b295480b24f
aniket3991 <3991aniketmishra@gmail.com>
2021-05-12 22:27:24 +0530

    error fixed for invalid ip or host

--- geooccupy.py
+++ geooccupy.py
@@ -13,54 +13,39 @@ $$    $$/ $$       |$$    $$/ $$    $$/ $$       |$$       |$$    $$/ $$    $$/ 
                                                                       $$ |      $$    $$/ 
                                 by KumarAtulJaiswal (@whoiskumaratul) $$/        $$$$$$/  
 """
-# try:
-#     print(header + "                   by KumarAtulJaiswal (@whoiskumaratul)\n")
-# except:
-#     print(header + "                           by @hackingtruthin\n")    
 
-try:
-    print(header)
-except:
-    print(header) 
-    
+print(header)
 
 print("\033[1;33;40m" +20*"*" +3*"/" +20*"*" +3*"/" +20*"*" +3*"/" +20*"*" +"\n" + "\033[0;0m")
 
-   
-
-
-print("[*] Tool - GeoOccupy: A geolocation finder tool via CLI\n" + "[*] Lang&Version - Python3\n" + "[*] Creation Date - May 8 2021 \n" + "[*] Please report any incorrect results at kumaratuljaiswal222@gmail.com\n")
-
+print("[*] Tool - GeoOccupy: A geolocation finder tool via CLI\n" +
+    "[*] Lang&Version - Python3\n" + "[*] Creation Date - May 8 2021 \n" +
+    "[*] Please report any incorrect results at kumaratuljaiswal222@gmail.com\n")
 
 print("\033[1;33;40m" +20*"*" +3*"/" +20*"*" +3*"/" +20*"*" +3*"/" +20*"*" +"\n" + "\033[0;0m")
 
 import socket
 import pygeoip
 import argparse
-from colorama import Fore, Back, Style
-import sys
 
 db1 = pygeoip.GeoIP('GeoIPASNum.dat')
 db2 = pygeoip.GeoIP('GeoLiteCity.dat')
 
 def printRecord(ip, given_args):
-    try:
-        database1 = db1.org_by_name(ip)
-        database2 = db2.record_by_name(ip)
-    except:
-        print("[**] Sorry, I can't locate that in my database")    
 
-    try:
-        country = database2['country_name']
-        city = database2['city']
-        continentName = database2['continent']
-        countrycode = database2['country_code']
-        postalcode = database2['postal_code']
-        timezone = database2['time_zone']
-        metrocode= database2['metro_code']
-        latitude = database2['latitude']
-        longitude = database2['longitude']
-        dma = database2['dma_code'] 
+    data = db2.record_by_name(ip) 
+
+    if data != None:
+        country = data['country_name']
+        city = data['city']
+        continentName = data['continent']
+        countrycode = data['country_code']
+        postalcode = data['postal_code']
+        timezone = data['time_zone']
+        metrocode= data['metro_code']
+        latitude = data['latitude']
+        longitude = data['longitude']
+        dma = data['dma_code'] 
 
         if given_args.country:
             print("[**] Country: " +str(country))
@@ -78,7 +63,7 @@ def printRecord(ip, given_args):
             print("[**] Postal: " +str(postalcode))
 
         if given_args.asn:
-            print("[**] ASN: " +str(database1))
+            print("[**] ASN: " +str(db1.org_by_name(ip)))
 
         if given_args.zone:
             print("[**] Zone: " +str(timezone))
@@ -95,33 +80,39 @@ def printRecord(ip, given_args):
         if given_args.dma:
             print("[**] DMA: " +str(dma))
 
-    except socket.gaierror:
-        pass 
+    else:
+        print("[**] Sorry, I can't locate that in my database")
 
 
 def main():
-    parser = argparse.ArgumentParser(description='GeoOccupy is command line argument tool. GeoOccupy means geolocation finder, which is used to find country, city, continent, country code, postal, time-zone via IP or Host. \n', prog='python3 geooccupy.py',  usage='%(prog)s [options]')
+    parser = argparse.ArgumentParser(description='GeoOccupy is command line argument tool. GeoOccupy means geolocation finder, '+ 
+        'which is used to find country, city, continent, country code, postal, time-zone via IP or Host. '+ 
+        '\n', prog='python3 geooccupy.py',  usage='%(prog)s [options]')
+
     parser.add_argument('-ip', action = 'store', dest='ip', help='IP or Host name that you want to extract info', required=True)
-    parser.add_argument('-c', action = 'store_true', dest='country', help='Find country name')#, required=True)
-    parser.add_argument('-cit', action = 'store_true', dest='city', help='Find city name' )#, required=True)
-    parser.add_argument('-con', action = 'store_true', dest='continent', help='Find continent')#, required=True)
-    parser.add_argument('-code', action = 'store_true', dest='code', help='Find country code')#, required=True)
-    parser.add_argument('-postal', action = 'store_true', dest='postal', help='Find postal code')#, required=True)
-    parser.add_argument('-asn', action = 'store_true', dest='asn', help='Find ASN number (Autonomous System Number)')#, required=True)
-    parser.add_argument('-zone', action = 'store_true', dest='zone', help='Find time-zone')#, required=True)
-    parser.add_argument('-metro', action = 'store_true', dest='metro', help='Find metro code')#, required=True)
+    parser.add_argument('-c', action = 'store_true', dest='country', help='Find country name')
+    parser.add_argument('-cit', action = 'store_true', dest='city', help='Find city name' )
+    parser.add_argument('-con', action = 'store_true', dest='continent', help='Find continent')
+    parser.add_argument('-code', action = 'store_true', dest='code', help='Find country code')
+    parser.add_argument('-postal', action = 'store_true', dest='postal', help='Find postal code')
+    parser.add_argument('-asn', action = 'store_true', dest='asn', help='Find ASN number (Autonomous System Number)')
+    parser.add_argument('-zone', action = 'store_true', dest='zone', help='Find time-zone')
+    parser.add_argument('-metro', action = 'store_true', dest='metro', help='Find metro code')
     parser.add_argument('-lat', action = 'store_true', dest='latitude', help='Find latitude')
     parser.add_argument('-long', action = 'store_true', dest='longitude', help='Find longitude')
     parser.add_argument('-dma', action = 'store_true', dest='dma', help='Find DMA code (Designated Market Area)')
     
     given_args = parser.parse_args()
-    
- 
-    ip = socket.gethostbyname(given_args.ip)   
+
+    try:
+        ip = socket.gethostbyname(given_args.ip)
+    except socket.gaierror:
+        print('Invalid IP or Host')
+        exit(0)
+
     print("[**] The IP that is resolved: " +ip)
     printRecord(str(ip), given_args)  
 
 
 
 main()
-
